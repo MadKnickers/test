d63359c3be532290d77c579f939d7f089174d698
unknown <mhd@ST-KILDA.scientia.com>
2016-04-28 14:19:27 +0100

    it's started raining, the first few animals are aboard

---
+++ NoahsArk3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoahsArk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] animals = System.IO.File.ReadAllLines(@"animals.txt");
+            foreach (var animal in animals)
+            {
+                System.Console.WriteLine(animal);
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
